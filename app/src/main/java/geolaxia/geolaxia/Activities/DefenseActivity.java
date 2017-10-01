45d9724a20913ad0ed1eece9a4af6629b93b25c6
ramirodoi <ramirodoi@gmail.com>
2017-10-01 18:59:13 -0300

    +Activity de defensa.

--- app/src/main/java/geolaxia/geolaxia/Activities/DefenseActivity.java
+++ app/src/main/java/geolaxia/geolaxia/Activities/DefenseActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import geolaxia.geolaxia.Model.Planet;
@@ -34,5 +35,10 @@ public class DefenseActivity extends MenuActivity {
 
         TextView cantCanonesActivos = (TextView)findViewById(R.id.defense_cant_canones_activos);
         cantCanonesActivos.setText("50");
+
+        NumberPicker np = (NumberPicker) findViewById(R.id.defense_cant_canones_construccion);
+        np.setMinValue(0);
+        np.setMaxValue(50);
+        np.setWrapSelectorWheel(true);
     }
 }
