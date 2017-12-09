cd02375ccfff5b08fed3d163bf612651e7340dd5
ramirodoi <ramirodoi@gmail.com>
2017-12-09 16:32:22 -0300

    * Fix.

--- app/src/main/java/geolaxia/geolaxia/Model/Adapters/NotificationListAdapter.java
+++ app/src/main/java/geolaxia/geolaxia/Model/Adapters/NotificationListAdapter.java
@@ -61,13 +61,13 @@ public class NotificationListAdapter extends RecyclerView.Adapter<NotificationLi
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         if(this.Notifications.get(position).getTipoNotificacion().equals("Ataque")){
-            holder.name.setText("Ataque - Llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT() + " del jugador " + this.Notifications.get(position).getPlayerName());
+            holder.name.setText("Ataque - Llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT() + " del jugador " + this.Notifications.get(position).getPlayerName() + ".");
             holder.name.setTextColor(Color.GREEN);
         } else if(this.Notifications.get(position).getTipoNotificacion().equals("Defensa")){
-            holder.name.setText("Defensa - El ataque del jugador " + this.Notifications.get(position).getPlayerName() + " desde el planeta " + this.Notifications.get(position).getPlanetNameO() + " llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT());
+            holder.name.setText("Defensa - El ataque del jugador " + this.Notifications.get(position).getPlayerName() + " desde el planeta " + this.Notifications.get(position).getPlanetNameO() + " llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT() + ".");
             holder.name.setTextColor(Color.RED);
         } else if(this.Notifications.get(position).getTipoNotificacion().equals("Colonización")){
-            holder.name.setText("Colonización - Llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT());
+            holder.name.setText("Colonización - Llega en " + this.ObtenerHora(this.Notifications.get(position).getTime()) + " al planeta " + this.Notifications.get(position).getPlanetNameT() + ".");
             holder.name.setTextColor(Color.YELLOW);
         } else {
             holder.name.setText("No hay notificaciones.");
@@ -75,25 +75,6 @@ public class NotificationListAdapter extends RecyclerView.Adapter<NotificationLi
         }
 
         holder.itemView.setBackgroundColor(Color.TRANSPARENT);
-        /*holder.itemView.setClickable(true);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                context.selectTargetPlanet(planets.get(position));
-                selectedPostion = position;
-                notifyDataSetChanged();
-            }
-        });*/
-
-        /*if(selectedPostion==position){
-            holder.itemView.setBackgroundColor(Color.WHITE);
-            holder.name.setTextColor(Color.BLACK);
-        }
-        else
-        {
-            holder.itemView.setBackgroundColor(Color.TRANSPARENT);
-            holder.name.setTextColor(Color.WHITE);
-        }*/
     }
 
     @Override
@@ -102,57 +83,20 @@ public class NotificationListAdapter extends RecyclerView.Adapter<NotificationLi
     }
 
     private String ObtenerHora(long arrivo) {
-        //String tiempoTexto = "";
         String tiempoExtension = "";
         long time = arrivo - System.currentTimeMillis();
-
-        //String dias = String.valueOf(TimeUnit.MILLISECONDS.toDays(time));
-        //String horas = String.valueOf(TimeUnit.MILLISECONDS.toHours(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toDays(time)));
-        //String minutos = String.valueOf(TimeUnit.MILLISECONDS.toMinutes(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(time)));
-        //String segundos = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time)));
 
         long dias = TimeUnit.MILLISECONDS.toDays(time);
         long horas = TimeUnit.MILLISECONDS.toHours(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toDays(time));
         long minutos = TimeUnit.MILLISECONDS.toMinutes(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(time));
         long segundos = TimeUnit.MILLISECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time));
 
-        tiempoExtension += (dias > 0) ? String.valueOf(dias) + ((dias == 1) ? " día" : " días") : "";
-        tiempoExtension += (horas > 0) ? ((dias > 0) ? " : " : "") + String.valueOf(horas) + ((horas == 1) ? " hora" : " horas") : " : ";
-        tiempoExtension += (minutos > 0) ? ((horas > 0) ? " : " : "") + String.valueOf(minutos) + ((minutos == 1) ? " min" : " mins") : " : ";
-        tiempoExtension += (segundos > 0) ? ((minutos > 0) ? " : " : "") + String.valueOf(segundos) + ((segundos == 1) ? " seg" : " segs") : " ";
+        tiempoExtension += (dias > 0) ? String.valueOf(dias) + ((dias == 1) ? " día" : " días") + " : " : "";
+        tiempoExtension += (horas > 0) ? String.valueOf(horas) + ((horas == 1) ? " hora" : " horas") + " : " : "";
+        tiempoExtension += (minutos > 0) ? String.valueOf(minutos) + ((minutos == 1) ? " min" : " mins") + " : " : "";
+        tiempoExtension += (segundos > 0) ? String.valueOf(segundos) + ((segundos == 1) ? " seg" : " segs") : "";
 
         tiempoExtension.trim();
-//        if (dias > 0) {
-//            if (dias == 1){
-//                tiempoTexto += String.valueOf(dias) + " día";
-//            } else {
-//                tiempoTexto += String.valueOf(dias) + " días";
-//            }
-//        }
-//
-//        if (horas > 0) {
-//            if (horas == 1){
-//                tiempoTexto += ((dias > 0) ? " : " : "") + String.valueOf(horas) + " hora";
-//            } else {
-//                tiempoTexto += ((dias > 0) ? " : " : "") + String.valueOf(horas) + " horas";
-//            }
-//        }
-//
-//        if (minutos > 0) {
-//            if (minutos == 1){
-//                tiempoTexto += ((horas > 0) ? " : " : "") + String.valueOf(minutos) + " min";
-//            } else {
-//                tiempoTexto += ((horas > 0) ? " : " : "") + String.valueOf(minutos) + " mins";
-//            }
-//        }
-//
-//        if (segundos > 0) {
-//            if (segundos == 1){
-//                tiempoTexto += ((minutos > 0) ? " : " : "") + String.valueOf(segundos) + " seg";
-//            } else {
-//                tiempoTexto += ((minutos > 0) ? " : " : "") + String.valueOf(segundos) + " segs";
-//            }
-//        }
 
         return(tiempoExtension);
     }
--- app/src/main/java/geolaxia/geolaxia/Model/Shield.java
+++ app/src/main/java/geolaxia/geolaxia/Model/Shield.java
@@ -3,22 +3,26 @@ package geolaxia.geolaxia.Model;
 import java.util.Date;
 
 public class Shield extends Military{
-    private boolean status;
+    private boolean IsActive;
 
-    public Shield(int id, String name, int constructinTime, Cost cost, Planet planet, int requiredLevel, Date enableDate, int attack, int defense){
+    public Shield (int id, String name, int constructinTime, Cost cost, Planet planet, int requiredLevel, Date enableDate, boolean isActive) {
+        super(id, name, constructinTime, cost, planet, requiredLevel, enableDate);
+        this.setStatus(isActive);
+    }
+    /*public Shield(int id, String name, int constructinTime, Cost cost, Planet planet, int requiredLevel, Date enableDate, int attack, int defense){
         super(id, name, constructinTime, cost, planet, requiredLevel, enableDate);
         this.Id = id;
-    }
+    }*/
 
-    public Shield(int id, String name, int constructinTime, geolaxia.geolaxia.Model.Cost cost, geolaxia.geolaxia.Model.Planet planet, int requiredLevel, Date enableDate) {
+    /*public Shield(int id, String name, int constructinTime, geolaxia.geolaxia.Model.Cost cost, geolaxia.geolaxia.Model.Planet planet, int requiredLevel, Date enableDate) {
         super(id, name, constructinTime, cost, planet, requiredLevel, enableDate);
-    }
+    }*/
 
     public boolean getStatus() {
-        return status;
+        return this.IsActive;
     }
 
-    public void setStatus(boolean status) {
-        status = status;
+    public void setStatus(boolean isActive) {
+        this.IsActive = isActive;
     }
 }
